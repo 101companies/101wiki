d68ca1ea6217b3993342faa37a9dfb68e431d312
Andrei Varanovich <dotnetby@gmail.com>
2012-09-20 17:11:09 +0200

    Updated Gemfile.lock

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,15 +32,33 @@ ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "id_rsa")]
 
 # if you want to clean up old releases on each deploy uncomment this:
 after "deploy:restart", "deploy:cleanup"
+before "deploy:update_code", "deploy:compress_assets"
+after "deploy:symlink", "deploy:upload_assets"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
+
+#TODO: try this to precompile assets https://gist.github.com/1477596
+
 # If you are using Passenger mod_rails uncomment this:
  namespace :deploy do
-   task :start do ; end
-   task :stop do ; end
-   task :restart, :roles => :app, :except => { :no_release => true } do
-     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-   end
+  desc "Compress assets in a local file"
+  task :compress_assets do
+    run_locally("rm -rf public/assets/*")
+    run_locally("bundle exec rake assets:precompile")
+    run_locally("touch assets.tgz && rm assets.tgz")
+    run_locally("tar zcvf assets.tgz public/assets/")
+    run_locally("mv assets.tgz public/assets/")
+  end
+  desc "Upload assets"
+  task :upload_assets do
+    upload("public/assets/assets.tgz", release_path + '/assets.tgz')
+    run "cd #{release_path}; tar zxvf assets.tgz; rm assets.tgz"
+  end
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
  end
