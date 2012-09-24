2c6069d1b5cb374a76fe6af865164c6c1cb4b295
Andrei Varanovich <dotnetby@gmail.com>
2012-09-24 11:33:15 +0200

    Migrated on bootstrap

--- spec/models/implementation_page.rb
+++ spec/models/implementation_page.rb
@@ -3,6 +3,27 @@
 require 'spec_helper'
 require 'pygments.rb'
 
+class WikiParser < WikiCloth::Parser
+
+    url_for do |page|
+      "javascript:alert('You clicked on: #{page}');"
+    end
+
+    link_attributes_for do |page|
+      { :href => url_for(page) }
+    end
+
+    template do |template|
+      puts template
+      "Hello {{{1}}}" if template == "hello"
+    end
+
+    external_link do |url,text|
+      "<a href=\"#{url}\" target=\"_blank\" class=\"exlink\">#{text.blank? ? url : text}</a>"
+    end
+
+end
+
 describe Page do
   it "should return a content for the implementation page" do
     @page = Page.new("101implementation:hadoop")
