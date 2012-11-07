d891816361974235a06cf23220461319d4691536
Andrei Varanovich <dotnetby@gmail.com>
2012-11-07 11:17:18 +0100

    Changes the sections API to return the raw text

--- app/models/page.rb
+++ app/models/page.rb
@@ -37,7 +37,13 @@ class Page
   end
 
   def sections
-    @wiki.sections.first.children.to_a
+    puts "sections:"
+    sec = []
+    @wiki.sections.first.children.each { |s| sec.push({'title' => s.title, 'wikitext' => s.wikitext})  }
+    return sec
+
+    #res = @wiki.sections.first.children.to_a.map{|s| return {title=> s.title, content => s.wikitext}}
+    #return res
   end
 
   def categories
